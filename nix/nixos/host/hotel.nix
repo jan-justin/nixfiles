3635d5fa5e652dabaeec9989ba2436508d1d39ff
Jan-Justin van Tonder <jan.justin.vtonder@gmail.com>
2025-03-23 14:39:22 +0100

    feat(system)!: upgrade hotel to all-amd platform

--- nix/nixos/host/hotel.nix
+++ nix/nixos/host/hotel.nix
@@ -15,7 +15,7 @@
     "xhci_pci"
   ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelModules = [ "kvm-amd" ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.enable = true;
@@ -25,19 +25,12 @@
   fileSystems."/".fsType = "ext4";
   fileSystems."/boot".device = "/dev/disk/by-label/BOOT";
   fileSystems."/boot".fsType = "vfat";
-  fileSystems."/mnt/baker".device = "/dev/disk/by-label/baker";
-  fileSystems."/mnt/baker".fsType = "ext4";
-  fileSystems."/mnt/easy".device = "/dev/disk/by-label/easy";
-  fileSystems."/mnt/easy".fsType = "ext4";
 
   hardware.bluetooth.enable = true;
-  hardware.cpu.intel.updateMicrocode = true;
+  hardware.cpu.amd.updateMicrocode = true;
   hardware.enableAllFirmware = true;
   hardware.enableRedistributableFirmware = true;
   hardware.graphics.enable = true;
-  hardware.graphics.extraPackages = with pkgs; [
-    intel-media-driver
-  ];
 
   networking.hostName = name;
 
