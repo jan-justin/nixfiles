b792c00b062b3e8061f49abfc5cc82841b638394
Jan-Justin van Tonder <jan.justin.vtonder@gmail.com>
2025-05-18 11:30:12 +0200

    feat(system): enable gamescope desktop session

--- nix/nixos/system/__profile/gaming.nix
+++ nix/nixos/system/__profile/gaming.nix
@@ -8,4 +8,14 @@
   programs.gamescope.capSysNice = true;
 
   programs.steam.enable = true;
+  programs.steam.gamescopeSession.enable = true;
+  programs.steam.gamescopeSession.args = [
+    "-W 3840"
+    "-w 3840"
+    "-H 1600"
+    "-h 1600"
+    "-r 119"
+    "--hdr-enabled"
+    "--mangoapp"
+  ];
 }
