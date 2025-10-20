06b0202248fcbff70672b892f73cf7cca350e0c3
Jan-Justin van Tonder <jan.justin.vtonder@gmail.com>
2025-10-20 19:52:38 +0200

    chore(user): address home-manager programs.ssh warning

--- nix/nixos/user/__profile/development.nix
+++ nix/nixos/user/__profile/development.nix
@@ -107,6 +107,19 @@
   programs.jujutsu.settings.ui.pager = ":builtin";
 
   programs.ssh.enable = true;
+  programs.ssh.enableDefaultConfig = false;
+  programs.ssh.matchBlocks."*" = {
+    forwardAgent = false;
+    addKeysToAgent = "no";
+    compression = false;
+    serverAliveInterval = 0;
+    serverAliveCountMax = 3;
+    hashKnownHosts = false;
+    userKnownHostsFile = "~/.ssh/known_hosts";
+    controlMaster = "no";
+    controlPath = "~/.ssh/master-%r@%n:%p";
+    controlPersist = "no";
+  };
   programs.ssh.extraConfig = ''
     Host *
       IdentityAgent ~/.1password/agent.sock 
